a4d3f6e6342e8a46eaf12f6b13e11b668a2c6142
𝐆𝛆𝛈𝛊𝛐𝛖𝛅ㅤ🏴‍☠️||鬼𝗚⽊𝛊神𝛐龙𝛅 🏴‍☠️ <misssqn@gmail.com>
2026-08-03 23:59:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,7 @@ class Config:
 
         self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NovaBot_Support")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+C7gu3hgdX3ljNTZh")
+        self.UPDATE = getenv("UPDATE", "https://t.me/Apple_Music_Update")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
