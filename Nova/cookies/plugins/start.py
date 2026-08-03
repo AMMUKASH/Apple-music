43cd073d4a170409da06281aa6f05d6fb26aa6f3
𝐆𝛆𝛈𝛊𝛐𝛖𝛅ㅤ🏴‍☠️||鬼𝗚⽊𝛊神𝛐龙𝛅 🏴‍☠️ <misssqn@gmail.com>
2026-08-04 01:43:13 +0530

    Change copyright and update module imports
    Updated copyright information and adjusted import paths.

--- Nova/cookies/plugins/start.py
+++ Nova/cookies/plugins/start.py
@@ -1,12 +1,12 @@
-# Copyright (c) 2025 AnonymousX1025
+# Copyright (c) 2025 CoderNova
 # Licensed under the MIT License.
-# This file is part of AnonXMusic
+# This file is part of Nova
 
 import asyncio
 from pyrogram import enums, filters, types
 
-from anony import app, config, db, lang
-from anony.helpers import buttons, utils
+from nova import app, config, db, lang
+from nova.helpers import buttons, utils
 
 
 @app.on_message(filters.command(["help"]) & filters.private & ~app.bl_users)
